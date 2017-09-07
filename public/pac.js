a6ebe1a92e25b39781f3bd95ad417169df8bf7dc
huzhifeng <huzhifeng@douyu.tv>
2017-09-07 09:37:42 +0800

    Update pac.js
    Signed-off-by: huzhifeng <huzhifeng@douyu.tv>

--- public/pac.js
+++ public/pac.js
@@ -332,8 +332,9 @@ function FindProxyForURL(url, host) {
         isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
         isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
         isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
-        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
+        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0")) {
         return DIRECT;
+    }
 
 
     // DEFAULT RULE: All other traffic, use below proxies, in fail-over order.
